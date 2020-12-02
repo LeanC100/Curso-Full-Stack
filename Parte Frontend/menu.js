d2839d776c4fab2a0f7fba89711ea9fc7b2d9b2f
LeanC1000 <leandrococa1999@gmail.com>
2020-12-02 01:02:54 -0300

    rama para el proyecto 1

--- Parte Frontend/menu.js
+++
@@ -1,8 +0,0 @@
-function menu() {
-    var x = document.getElementById("navResponsive");
-    if (x.className === "nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav";
-    }
-}
